da85cebd2242b70fa58561c4edf70f228e62caf5
naufaladrna08 <naufaladrn90@gmail.com>
2022-06-09 09:56:48 +0700

    added common header

---
+++ include/Common.hpp
@@ -0,0 +1,21 @@
+/**
+ * @file Common.hpp
+ * @author Naufal Adriansyah (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-06-09
+ * 
+ * This file contains all necessary variable and constants.
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#ifndef COMMON_H
+#define COMMON_H
+
+#define APP_NAME "Jin"
+#define GTK_NAME "org.kincirangin.jin"
+#define TEMPLATE_PATH ".jin-templates/"
+
+#endif
