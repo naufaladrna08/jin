d8db56ad09cab4823b0a7a3b76ea8dc84bf684d8
naufaladrna08 <naufaladrn90@gmail.com>
2022-06-09 11:44:19 +0700

    added completion

--- include/MainWindow.hpp
+++ include/MainWindow.hpp
@@ -1,12 +1,21 @@
 #ifndef MAIN_WINDOWN_HPP
 #define MAIN_WINDOWN_HPP
 
-#include <gtkmm/entry.h>
-#include <gtkmm/window.h>
+#include <gtkmm.h>
 #include <iostream>
+#include <Utils.h>
 
 /* Load X11 Linux */
 #include <X11/Xlib.h>
+
+extern tfile_t root;
+
+enum {
+  COL_NAME = 0,
+  COL_PATH,
+  NUM_COLS
+};
+
 
 class MainWindow : public Gtk::Window {
   public:
@@ -15,6 +24,8 @@ class MainWindow : public Gtk::Window {
 
   protected:
     Gtk::Entry m_textbox;
+    GtkListStore *store;
+    GtkTreeModel* populateCompletion();
 
   private:
     int m_screenw, m_screenh;
--- src/MainWindow.cxx
+++ src/MainWindow.cxx
@@ -13,6 +13,17 @@ MainWindow::MainWindow() {
   /* Disable window's frame */
   Gtk::Window::set_decorated(false);
 
+  GtkEntryCompletion *completion;
+  GtkTreeModel *completion_model;
+
+  completion = gtk_entry_completion_new();
+  gtk_entry_set_completion (GTK_ENTRY(m_textbox.gobj()), completion);
+
+  completion_model = this->populateCompletion();
+  gtk_entry_completion_set_model(completion, completion_model);
+  
+  gtk_entry_completion_set_text_column(completion, 0);
+
   /* Set window's size */
   Gtk::Window::set_size_request(m_screenw / 3, 32);
   Gtk::Window::move((m_screenw / 2) - (m_screenw / 3) / 2, m_screenh / 2 - 64);
@@ -24,4 +35,19 @@ MainWindow::MainWindow() {
 
 MainWindow::~MainWindow() {
 
+}
+
+GtkTreeModel* MainWindow::populateCompletion() {
+  ls_home();
+  store = gtk_list_store_new(2, G_TYPE_STRING, G_TYPE_STRING);
+  GtkTreeIter iter;
+
+  tfile_t c = root;
+	while (c != nullptr) {
+		gtk_list_store_append(store, &iter);
+    gtk_list_store_set(store, &iter, COL_NAME, c->data, COL_PATH, c->data, -1);
+		c = c->next;
+	}
+
+  return GTK_TREE_MODEL(store);
 }
