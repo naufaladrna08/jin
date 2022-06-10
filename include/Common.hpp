47eba28caaa2ff95f4a63405c9465bf7e791edb2
naufaladrna08 <naufaladrn90@gmail.com>
2022-06-10 17:47:49 +0700

    added home directory define

--- include/Common.hpp
+++ include/Common.hpp
@@ -14,9 +14,13 @@
 #ifndef COMMON_H
 #define COMMON_H
 
+#include <glib/gutils.h>
+
 #define APP_NAME "Jin"
 #define GTK_ID "org.kincirangin.jin"
 #define TEMPLATE_PATH ".jin-templates/"
 #define KEY_SHORTCUT "<Ctrl>D"
+
+#define HOME g_get_home_dir()
 
 #endif
