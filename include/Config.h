8524de109ee3aff6b1bad092a65326981d3ed412
naufaladrna08 <naufaladrn90@gmail.com>
2022-06-24 21:48:11 +0700

    add config header

---
+++ include/Config.h
@@ -0,0 +1,15 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <Common.h>
+#include <Utils.h>
+
+typedef char* string;
+typedef const char* cstring;
+
+string get_config_string(string config_name);
+int get_config_int(string config_name);
+
+#endif
