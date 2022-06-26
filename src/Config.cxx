b455d2aabbc909b6e73ca7730fb22b1a2518e8e0
naufaladrna08 <naufaladrn90@gmail.com>
2022-06-26 22:56:01 +0700

    added get_config_string

---
+++ src/Config.cxx
@@ -0,0 +1,36 @@
+#include <Config.h>
+
+string get_config_string(string config_name) {
+  string path = get_home_directory();
+  string result = "\0";
+  FILE* fptr;
+
+  strcat(path, "/");
+  strcat(path, CONFIG_NAME);
+
+  fptr = fopen(path, "r");
+
+  if (fptr == NULL) {
+    printf("Configuration file is not found\n");
+    exit(1);
+  }
+  char buffer[1024];
+
+  while (fgets(buffer, sizeof(buffer), fptr)) {
+    string key = strtok(buffer, " = ");
+
+    if (strcmp(key, config_name) == 0) {
+      while (key != NULL) {
+        result = key;
+        key = strtok(NULL, " = ");
+      }
+    }
+  }
+
+  fclose(fptr);
+  return result;
+}
+
+int get_config_int(string config_name) { 
+  return 1;
+}
